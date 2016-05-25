0a6a75dfa23f3a5faf36bb5373f8ae0acb1c2df9
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-25 10:17:49 -0400

    capital one summit files

---
+++ android/android-demo/app/src/androidTest/java/theanimasian/c1ses2016arduino/ApplicationTest.java
@@ -0,0 +1,13 @@
+package theanimasian.c1ses2016arduino;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
