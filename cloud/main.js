cfe7d7c521fecccc2a7021c30ccd30618b61c629
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-26 18:29:37 -0400

    remove redundant master key

--- cloud/main.js
+++ cloud/main.js
@@ -43,8 +43,7 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
       console.log("#### PUSH OK");
     }, error: function(error) {
       console.log("#### PUSH ERROR" + error.message);
-    }, 
-    useMasterKey: true
+    } 
   });
 
   response.success('success');
