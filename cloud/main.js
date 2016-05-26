4230d104c9cbd709d2d3ecfe6b54a4b8cf5ab72c
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-26 16:38:15 -0400

    added sos endpoint

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
-
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
+});
+
+Parse.Cloud.define('sos', function(req, res) {
+  var query = new Parse.Query(Parse.Installation);
+
+  Parse.Push.send({
+    where: query, // Set our Installation query
+    data: {
+      alert: "Broadcast to everyone"
+    }
+  }, {
+    success: function() {
+      // Push was successful
+      console.log("success");
+    },
+    error: function(error) {
+      console.log("error")
+      console.log(error);
+    }
+  });
 });
