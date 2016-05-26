e5b7035350c0a0fbe30410ea4174a6a4e0bb06b4
Sahaj Bhatt <sahajb97@gmail.com>
2016-05-26 13:52:17 -0400

    Added map view to home view

--- ios/CashDash/CDHomeScreenController.swift
+++ ios/CashDash/CDHomeScreenController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import MapKit
 import Parse
 
 class CDHomeScreenController: CDBaseViewController {
 	
+    static let screenSize = UIScreen.mainScreen().bounds
+    let screenWidth = screenSize.width
+    let screenHeight = screenSize.height
+
     //buttons
     let cashBtn = UIButton()
     let dashBtn = UIButton()
@@ -19,22 +24,25 @@ class CDHomeScreenController: CDBaseViewController {
     //labels
     let logo = UILabel()
     
+    //text fields
+    let cashAmt = UITextField()
+    
     //views
     let navigationStack = UIStackView()
+    
+    //maps
+    let atmMap = MKMapView()
+    let sosMap = MKMapView()
     
     func configureButtons() {
         
         
         cashBtn.setTitle("Cash", forState: .Normal)
-        cashBtn.backgroundColor = UIColor.greenColor()
+        cashBtn.backgroundColor = UIColor(r: 76, g: 173, b: 0, a: 1)
         dashBtn.setTitle("Dash", forState: .Normal)
-        dashBtn.backgroundColor = UIColor.greenColor()
+        dashBtn.backgroundColor = UIColor(r: 76, g: 173, b: 0, a: 1)
         sosBtn.setTitle("SOS", forState: .Normal)
-        sosBtn.backgroundColor = UIColor.greenColor()
-        
-        let screenSize = UIScreen.mainScreen().bounds
-        let screenWidth = Int(screenSize.width)
-        let screenHeight = Int(screenSize.height)
+        sosBtn.backgroundColor = UIColor(r: 76, g: 173, b: 0, a: 1)
         
         cashBtn.frame = CGRect(x: 0, y: 0, width: screenWidth/2, height: 40)
         dashBtn.frame = CGRect(x: screenWidth/2, y: 0, width: screenWidth/2, height: 40)
@@ -68,15 +76,20 @@ class CDHomeScreenController: CDBaseViewController {
         let viewsDict = [
             "logo": logo,
             
-            "sb": sosBtn,
+//            "sb": sosBtn,
             
-            "ns": navigationStack
+            "ns": navigationStack,
+            
+            "am": atmMap,
         ]
         
         self.view.prepareViewsForAutoLayout(viewsDict)
         
         self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-20-[logo]-20-|", views: viewsDict))
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-40-[logo(==40)]-20-[ns]", views: viewsDict))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-40-[logo(==40)]-20-[ns][am]|", views: viewsDict))
+        
+//        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:[am(==120)]", views: viewsDict))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[am]|", views: viewsDict))
         
         self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[ns]|", views: viewsDict))
         self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:[ns(==40)]", views: viewsDict))
@@ -87,24 +100,47 @@ class CDHomeScreenController: CDBaseViewController {
         
 //        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-40-[logo(==40)]", views: viewsDict))
         
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[sb]|", views: viewsDict))
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:[sb(==40)]|", views: viewsDict))
+//        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[sb]|", views: viewsDict))
+//        self.view.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:[sb(==40)]|", views: viewsDict))
+        
+        self.sosBtn.frame = CGRect(x: 0, y: self.screenHeight - 40, width: self.screenWidth, height: 40)
+        self.view.addSubview(sosBtn)
         
     }
     
     func showAtmMap() {
-        cashBtn.backgroundColor = UIColor(red: 0, green: 255, blue: 0, alpha: 0.5)
+        cashBtn.backgroundColor = UIColor(r: 51, g: 102, b: 0, a: 1)
+        dashBtn.backgroundColor = UIColor(r: 76, g: 173, b: 0, a: 1)
+        
+        //Show atm markers
+        UIView.animateWithDuration(CDUIConstants.animationDuration, animations: { [unowned self] in
+            //self.sosBtn.setTitle("Request SOS", forState: .Normal)
+            self.sosBtn.frame = CGRect(x: 0, y: self.screenHeight - 40, width: self.screenWidth, height: 40)
+            
+            }, completion: { [unowned self] (complete) in
+                
+            })
+
     }
     
     func showSosMap() {
+        dashBtn.backgroundColor = UIColor(r: 51, g: 102, b: 0, a: 1)
+        cashBtn.backgroundColor = UIColor(r: 76, g: 173, b: 0, a: 1)
         
+        //Show users in area
+        UIView.animateWithDuration(CDUIConstants.animationDuration, animations: { [unowned self] in
+            //self.sosBtn.setTitle("Request SOS", forState: .Normal)
+            self.sosBtn.frame = CGRect(x: 0, y: self.screenHeight, width: self.screenWidth, height: 40)
+            
+            }, completion: { [unowned self] (complete) in
+                
+            })
     }
     
     func startSosRequest() {
-        print("yo")
         UIView.animateWithDuration(CDUIConstants.animationDuration, animations: { [unowned self] in
             self.sosBtn.setTitle("Request SOS", forState: .Normal)
-            
+            self.sosBtn.frame = CGRect(x: 0, y: self.screenHeight - 120, width: self.screenWidth, height: 40)
             
             }, completion: { [unowned self] (complete) in
                 
@@ -114,7 +150,8 @@ class CDHomeScreenController: CDBaseViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        print("hi")
+        
+        showAtmMap()
     }
 
 	
