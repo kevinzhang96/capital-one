9ae42b947b30f9a457680a623f76bac974a70d57
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-27 08:38:28 -0400

    fixed badge number

--- ios/CashDash/AppDelegate.swift
+++ ios/CashDash/AppDelegate.swift
@@ -27,6 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		})
 		Parse.initializeWithConfiguration(config)
 		
+		UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+		
 		// register for notifications
 		let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
 		application.registerUserNotificationSettings(settings)
