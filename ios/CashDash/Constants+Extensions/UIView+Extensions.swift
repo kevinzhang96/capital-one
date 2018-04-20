cb46cee07004a6ee9461a55cfaf6d0c703fcbd51
Kevin Zhang <kevin.zhang500@gmail.com>
2018-04-20 17:04:55 -0400

    added cashdash directory

--- ios/CashDash/Constants+Extensions/UIView+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-extension UIView {
-	func prepareViewsForAutoLayout(viewsDict: [String: UIView]) {
-		let _ = Array(viewsDict.values).map({$0.translatesAutoresizingMaskIntoConstraints = false; self.addSubview($0)})
-	}
-}
